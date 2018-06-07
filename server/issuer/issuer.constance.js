87ef33571489f3a28daf4eb6484bd25c8522dbdb
mcapeta <marin.capeta@gmail.com>
2018-06-07 08:25:20 +0200

    Find Issuer's image by provided or default path

--- server/issuer/issuer.constance.js
+++ server/issuer/issuer.constance.js
@@ -4,16 +4,22 @@ const glob = require('glob');
 const issuer = require('../../.issuer-rc.json');
 const path = require('path');
 
-function findImage() {
-  const pattern = path.resolve(__dirname,
-    '../assets/issuer-image.@(svg|jpg|jpeg|png)');
+function findImage(imagePath) {
+  let pattern;
+  if (imagePath) {
+    pattern = path.resolve(__dirname, '../../', imagePath);
+  } else {
+    pattern = path.resolve(__dirname,
+      '../assets/issuer-image.@(svg|jpg|jpeg|png)');
+  }
+
   const files = glob.sync(pattern);
   if (files.length > 1) {
     throw Error(`Found more than one image matching '${pattern}' pattern!`);
   }
-  return (files.length === 0) ? '' : files[0];
+  return (files.length === 1) ? files[0] : '';
 }
 
-if (!issuer.imagePath) issuer.imagePath = findImage();
+issuer.imagePath = findImage(issuer.imagePath);
 
 module.exports = issuer;
