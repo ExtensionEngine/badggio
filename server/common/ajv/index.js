46dfb741ffdb27274b6edb2d0b18dc5fb1c5df3f
mcapeta <marin.capeta@gmail.com>
2018-06-12 10:29:55 +0200

    Rename keyword 'path' to 'fileExists'

--- server/common/ajv/index.js
+++ server/common/ajv/index.js
@@ -3,10 +3,10 @@
 const Ajv = require('ajv');
 const fs = require('fs');
 
-function validate(path) {
+function fileExists(path) {
   if (fs.existsSync(path)) return true;
 
-  validate.errors = [{
+  fileExists.errors = [{
     keyword: 'path',
     message: `File at path "${path}" should exist!`
   }];
@@ -18,7 +18,7 @@ ajv.addKeyword('path', {
   type: 'string',
   errors: true,
   schema: false,
-  validate: validate
+  validate: fileExists
 });
 
 module.exports = ajv;
