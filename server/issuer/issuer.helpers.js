0a9ffea979edba86ce9d5e29da31d8af4f3f24f0
mcapeta <marin.capeta@gmail.com>
2018-06-12 16:49:13 +0200

    Add helper methods

---
+++ server/issuer/issuer.helpers.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+const { issuer } = require('../config');
+
+function profile() {
+  return {
+    id: issuer.issuerUrl,
+    type: 'Issuer',
+    name: issuer.name,
+    telephone: issuer.telephone,
+    description: issuer.description,
+    image: issuer.imageUrl,
+    email: issuer.email,
+    publicKey: publicKey(),
+    verificationObject: verificationObject(),
+    revocationList: revocationList()
+  };
+}
+
+function publicKey() {
+  if (!issuer.publicKey || !issuer.privateKey) return null;
+  return {
+    id: issuer.publicKeyUrl,
+    type: 'CryptographicKey',
+    owner: issuer.issuerUrl,
+    publicKeyPem: fs.readFileSync(issuer.publicKey, 'utf8')
+  };
+}
+
+function revocationList() {
+  // TODO: add revokedAssertions property
+  return {
+    type: 'RevocationList',
+    issuer: issuer.issuerUrl
+  };
+}
+
+function verificationObject() {
+  if (!issuer.publicKey || !issuer.privateKey) {
+    return { type: 'HostedBadge' };
+  }
+  return {
+    type: 'SignedBadge',
+    creator: issuer.publicKeyUrl
+  };
+}
+
+module.exports = {
+  profile,
+  publicKey,
+  revocationList,
+  verificationObject
+};
