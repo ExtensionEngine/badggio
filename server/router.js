53cd8db0fdd19f29f9135b3b44672aa2620104e2
mcapeta <marin.capeta@gmail.com>
2018-06-12 15:44:13 +0200

    Rename root router to badging

--- server/router.js
+++ server/router.js
@@ -4,12 +4,12 @@ const express = require('express');
 const user = require('./user');
 
 const api = express.Router();
-const root = express.Router();
+const badging = express.Router();
 const router = express.Router();
 
 api.use(user.path, user.router);
 
-router.use(root);
+router.use(badging);
 router.use('/api/v1', api);
 
 module.exports = router;
