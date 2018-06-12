ff77074e8cc0ff53ac36b859cc76b6597a83b674
Nediljko Krstic <nediljko@extensionengine.com>
2018-06-12 13:52:11 +0200

    Add transactions to badge controllers

--- server/badge-class/badge-class.controller.js
+++ server/badge-class/badge-class.controller.js
@@ -1,49 +1,68 @@
 'use strict';
 
 const { createError } = require('../common/errors');
-const { BadgeClass } = require('../common/database');
+const { BadgeClass, sequelize } = require('../common/database');
 const createStorage = require('../common/storage');
 const config = require('../config');
 const hasha = require('hasha');
 const HttpStatus = require('http-status');
 const map = require('lodash/map');
 const pick = require('lodash/pick');
+const set = require('lodash/set');
 
-const { NOT_FOUND } = HttpStatus;
+const { NOT_FOUND, SERVICE_UNAVAILABLE } = HttpStatus;
 const inputAttrs = ['name', 'description', 'criteriaNarrative', 'imageCaption',
   'imageAuthorId', 'tags'];
 const storage = createStorage(config.storage);
 
 function create({ body }, res) {
-  return BadgeClass.findOne({ where: { name: body.name } })
-    .then(badge => !badge || createError(NOT_FOUND, 'Badge already exists!'))
-    .then(() => {
-      const imageHash = hasha(body);
-      return BadgeClass.create({ ...pick(body, inputAttrs), imageHash });
-    }).then(badge => {
-      return storage.setItem({ key: `${badge.id}.${body.imageType}` })
-        .then(() => badge);
-    }).then(badge => res.jsend.success(badge.profile));
+  return sequelize.transaction((t) => {
+    const transaction = { transaction: t };
+    const newBadge = { ...pick(body, inputAttrs) };
+
+    return BadgeClass.findOne({ where: { name: body.name } })
+      .then(badge => !badge || createError(NOT_FOUND, 'Badge already exists!'))
+      .then(() => set(newBadge, 'imageHash', hasha(body.image)))
+      .then(() => BadgeClass.create(newBadge, transaction))
+      .then(badge => {
+        const key = `${badge.id}.${body.imageType}`;
+        return storage.setItem({ key }).then(() => badge);
+      });
+  }).then(badge => res.jsend.success(badge.profile))
+    .catch(() => {
+      const error = createError(SERVICE_UNAVAILABLE, 'Badge create failed!');
+      return res.jsend.fail(error);
+    });
 }
 
+// TODO: finish this when we have created some badges
 function list({ query: { email, emailLike, role } }, res) {
   return BadgeClass.findAll()
     .then(badges => res.jsend.success(map(badges, 'profile')));
 }
 
 function patch({ params, body }, res) {
-  return BadgeClass.findById(params.id, { paranoid: false })
-    .then(badge => badge || createError(NOT_FOUND, 'Badge does not exist!'))
-    .then(badge => {
-      const newValues = { ...pick(body, inputAttrs) };
-      const imageHash = hasha(body);
-      if (imageHash === badge.imageHash) return { badge, newValues };
-      newValues.imageHash = imageHash;
-      return storage.setItem({ key: `${badge.id}.${body.imageType}` })
-        .then(() => ({ badge, newValues }));
-    })
-    .then(({ badge, newValues }) => badge.update(newValues))
-    .then(badge => res.jsend.success(badge.profile));
+  return sequelize.transaction((t) => {
+    const transaction = { transaction: t };
+    const newValues = { ...pick(body, inputAttrs) };
+
+    return BadgeClass.findById(params.id, { paranoid: false })
+      .then(badge => badge || createError(NOT_FOUND, 'Badge does not exist!'))
+      .then(badge => {
+        set(newValues, 'imageHash', hasha(body.image));
+        return badge.update(newValues, transaction);
+      })
+      .then(badge => {
+        const key = `${badge.id}.${body.imageType}`;
+        return !storage.fileExists(badge.imageHash)
+          ? storage.setItem({ key }).then(() => badge)
+          : badge;
+      });
+  }).then(badge => res.jsend.success(badge.profile))
+  .catch(() => {
+    const error = createError(SERVICE_UNAVAILABLE, 'Badge patch failed!');
+    return res.jsend.fail(error);
+  });
 }
 
 module.exports = {
