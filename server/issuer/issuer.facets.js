7839be74e4b565b4f8e4af68d264c324fbd5f765
mcapeta <marin.capeta@gmail.com>
2018-06-13 13:35:48 +0200

    Simplify conditional property loading

--- server/issuer/issuer.facets.js
+++ server/issuer/issuer.facets.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const { issuer } = require('../config');
 
 function profile() {
-  const profile = {
+  return {
     id: issuer.issuerUrl,
     type: 'Issuer',
     name: issuer.name,
@@ -12,14 +12,10 @@ function profile() {
     description: issuer.description,
     image: issuer.imageUrl,
     email: issuer.email,
+    publicKey: (issuer.publicKey) ? publicKey() : undefined,
     verification: verificationObject(),
     revocationList: revocationList()
   };
-
-  const publicKey = publicKey();
-  if (publicKey) profile.publicKey = publicKey;
-
-  return profile;
 }
 
 function publicKey() {
