f72239ffe632383adb8b62f0ddcc3c622e572b41
mcapeta <marin.capeta@gmail.com>
2018-06-18 12:33:18 +0200

    Mount Issuer routers to main router

--- server/router.js
+++ server/router.js
@@ -2,12 +2,18 @@
 
 const express = require('express');
 const user = require('./user');
+const issuer = require('./issuer');
 
 const api = express.Router();
 const badging = express.Router();
 const router = express.Router();
 
-api.use(user.path, user.router);
+api
+  .use(user.path, user.router)
+  .use(issuer.path, issuer.api);
+
+badging
+  .use(issuer.path, issuer.badging);
 
 router.use('/', badging);
 router.use('/api/v1', api);
