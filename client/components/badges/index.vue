77b10b7cffdadca83b921bff8212aa7bc281d52a
Nediljko Krstic <nediljko@extensionengine.com>
2018-06-21 14:45:44 +0200

    Add table on frontend

--- client/components/badges/index.vue
+++ client/components/badges/index.vue
@@ -6,6 +6,34 @@
       class="btn-create button is-primary is-pulled-right">
       Create
     </button>
+    <table class="table is-fullwidth is-hoverable">
+      <thead>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Image</th>
+        <th>Image caption</th>
+        <th>Image author</th>
+        <th>Criteria</th>
+        <th>Tags</th>
+        <th></th>
+      </thead>
+      <tbody>
+        <tr v-for="badge in badges" :key="badge._cid">
+          <td>{{ badge.name }}</td>
+          <td>{{ badge.description }}</td>
+          <td><img :src="badge.image"/></td>
+          <td>{{ badge.imageCaption }}</td>
+          <td>{{ badge.imageAuthorIri }}</td>
+          <td>{{ badge.criteriaNarrative }}</td>
+          <td>{{ badge.tags | readable }}</td>
+          <td>
+            <button @click="edit(badge)" class="button is-small is-pulled-right is-outlined">
+              <span class="mdi mdi-pencil"></span>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <badge-modal
       :show="showModal"
       :badgeData="context"
@@ -32,10 +60,22 @@ export default {
     create() {
       this.context = null;
       this.showModal = true;
+    },
+    edit(badge) {
+      this.context = badge;
+      this.showModal = true;
+    },
+    tags(badge) {
+      return badge.tags.join(', ');
     }
   },
   mounted() {
     this.fetch();
+  },
+  filters: {
+    readable(tags) {
+      return tags.join(', ');
+    }
   },
   components: { BadgeModal }
 };
