5d7fba0079192f2e9cf1ab86de572ca5e4642ca8
Nediljko Krstic <nediljko@extensionengine.com>
2018-06-26 13:39:28 +0200

    Add encodeImage middleware, remove imageHash from inputArgs

--- server/badge-class/badge-class.controller.js
+++ server/badge-class/badge-class.controller.js
@@ -4,30 +4,35 @@ const { createError } = require('../common/errors');
 const { BadgeClass, sequelize } = require('../common/database');
 const hasha = require('hasha');
 const HttpStatus = require('http-status');
+const map = require('lodash/map');
 const pick = require('lodash/pick');
 
 const { NOT_FOUND } = HttpStatus;
 const inputAttrs = ['name', 'description', 'criteriaNarrative', 'imageCaption',
-  'imageHash', 'imageAuthorIri', 'tags'];
+  'imageAuthorIri', 'tags'];
 
-function create({ body }, res) {
+function create(req, res) {
+  const { body, decodedImage: { imageHash } } = req;
   return sequelize.transaction(transaction => {
-    return BadgeClass.create({ ...pick(body, inputAttrs) }, transaction)
+    return BadgeClass.create({ ...pick(body, inputAttrs, imageHash) }, transaction)
       .then(badge => badge.storeImage(body));
   }).then(badge => res.jsend.success(badge));
 }
 
 // TODO: finish this when we have created some badges
-function list({ query: { email, emailLike, role } }, res) {
-  return BadgeClass.findAll()
-    .then(badges => res.jsend.success(badges));
+function list(req, _, next) {
+  return BadgeClass.findAll().then(badges => {
+    req.locals = { badges };
+    next();
+  });
 }
 
-function patch({ params, body }, res) {
+function patch(req, res) {
+  const { body, decodedImage: { imageHash }, params } = req;
   return sequelize.transaction(transaction => {
     return BadgeClass.findById(params.id, { paranoid: false })
       .then(badge => badge || createError(NOT_FOUND, 'Badge does not exist!'))
-      .then(badge => badge.update({ ...pick(body, inputAttrs) }, transaction))
+      .then(badge => badge.update({ ...pick(body, inputAttrs, imageHash) }, transaction))
       .then(badge => badge.storeImage(body));
   }).then(badge => res.jsend.success(badge));
 }
@@ -41,17 +46,26 @@ function patch({ params, body }, res) {
  * @const {string} imageHash - Hashed base64 data.
  */
 
-function decodeImage({ body }, res, next) {
+function decodeImage(req, res, next) {
+  const { body } = req;
   const extension = body.image.split(';')[0].split('/')[1];
   const base64data = body.image.split(',')[1];
-  const imageHash = hasha(base64data);
-  Object.assign(body, { base64data, extension, imageHash });
+  const imageHash = hasha(base64data + extension);
+  const decodedImage = { base64data, extension, imageHash };
+  Object.assign(req, decodedImage);
   return next();
+}
+
+function encodeImage({ locals: { badges } }, res) {
+  if (!Array.isArray(badges)) badges = [badges];
+  return Promise.all(map(badges, badge => badge.getImage()))
+    .then(() => res.jsend.success(badges));
 }
 
 module.exports = {
   create,
   decodeImage,
+  encodeImage,
   list,
   patch
 };
