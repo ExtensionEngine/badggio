249b7b40b9f1c02dda6aede96d4d07f6020c148d
Nediljko Krstic <nediljko@extensionengine.com>
2018-06-27 11:51:20 +0200

    Add server side badge validation

--- server/badge-class/index.js
+++ server/badge-class/index.js
@@ -2,6 +2,7 @@
 
 const auth = require('../common/auth').authenticate('jwt');
 const ctrl = require('./badge-class.controller');
+const validate = require('./badge-class.validator');
 const router = require('express').Router();
 
 const { create, encodeImage, decodeImage, list, patch } = ctrl;
@@ -9,6 +10,7 @@ const { create, encodeImage, decodeImage, list, patch } = ctrl;
 router
   .use(auth)
   .get('/', list, encodeImage)
+  .use(validate)
   .post('/', decodeImage, create)
   .patch('/:id', decodeImage, patch);
 
