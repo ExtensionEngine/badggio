73c8ef3bdf1eb732b44c9b494816824435cff6e0
mcapeta <marin.capeta@gmail.com>
2018-07-02 14:54:17 +0200

    Change Recipient seed

--- server/common/database/seeds/recipients.js
+++ server/common/database/seeds/recipients.js
@@ -1,24 +1,27 @@
 'use strict';
 
 const crypto = require('crypto');
+const hasha = require('hasha');
 
 const now = new Date();
+const salt = crypto.randomBytes(32).toString('hex');
+
 const recipients = [{
   email: 'non_hashed@example.org',
-  hashed: false,
+  hash: null,
   salt: null,
   created_at: now,
   updated_at: now
 }, {
   email: 'hashed_non_salted@example.org',
-  hashed: true,
+  hash: sha256('hashed_non_salted@example.org'),
   salt: null,
   created_at: now,
   updated_at: now
 }, {
   email: 'hashed_and_salted@example.org',
-  hashed: true,
-  salt: crypto.randomBytes(32).toString('hex'),
+  hash: sha256('hashed_and_salted@example.org' + salt),
+  salt,
   created_at: now,
   updated_at: now
 }];
@@ -31,3 +34,7 @@ module.exports = {
     return queryInterface.bulkDelete('recipient', null, {});
   }
 };
+
+function sha256(value) {
+  return 'sha256$' + hasha(value, { algorithm: 'sha256' });
+}
