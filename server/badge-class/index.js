506272a86ccd4f6d49942ac5d39e20f0661d94c8
Nediljko Krstic <nediljko@extensionengine.com>
2018-07-02 14:42:19 +0200

    Remove joi server side validation

--- server/badge-class/index.js
+++ server/badge-class/index.js
@@ -2,7 +2,6 @@
 
 const auth = require('../common/auth').authenticate('jwt');
 const ctrl = require('./badge-class.controller');
-const validate = require('./badge-class.validator');
 const router = require('express').Router();
 
 const { create, encodeImage, decodeImage, list, patch } = ctrl;
@@ -10,7 +9,6 @@ const { create, encodeImage, decodeImage, list, patch } = ctrl;
 router
   .use(auth)
   .get('/', list, encodeImage)
-  .use(validate)
   .post('/', decodeImage, create)
   .patch('/:id', decodeImage, patch);
 
