0ea7e524b754dea01556f80b6e495e714aae8d7f
mcapeta <marin.capeta@gmail.com>
2018-07-03 11:57:12 +0200

    Move 'identityObject' from model to facet

--- server/recipient/recipient.facets.js
+++ server/recipient/recipient.facets.js
@@ -1,19 +1,13 @@
 'use strict';
 
-const hasha = require('hasha');
 const pickBy = require('lodash/pickBy');
 
-function hash(email, salt) {
-  if (!salt) salt = '';
-  return 'sha256$' + hasha(email + salt, { algorithm: 'sha256' });
-}
-
-function identityObject({ email, hashed, salted, salt }) {
-  return Object.assign(
-    { hashed, type: 'email' },
-    { identity: (salted || hashed) ? hash(email, salt) : email },
-    pickBy({ salt })
-  );
+function identityObject({ email, hash, salt }) {
+  return Object.assign({
+    email: hash || email,
+    hashed: Boolean(hash),
+    type: 'email'
+  }, pickBy({ salt }));
 }
 
 module.exports = {
