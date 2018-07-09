f56206537984136e550d735ad448999f7380536e
Nediljko Krstic <nediljko@extensionengine.com>
2018-07-09 14:13:07 +0200

    Fix setting badge id

--- server/badge-class/badge-class.facets.js
+++ server/badge-class/badge-class.facets.js
@@ -3,7 +3,6 @@
 const paths = require('./badge-class.paths');
 const pickBy = require('lodash/pickBy');
 const { base: facetBase } = require('../common/facets');
-const { issuer } = require('../config');
 const { SERVER_URL } = process.env;
 
 const root = SERVER_URL + paths.root;
@@ -22,12 +21,11 @@ function bakedImageIri({ id }) {
   return `${root}/${id}${paths.bakedImage}`;
 }
 
-function id({ id, uuid }) {
-  return issuer.publicKey ? `urn:uuid:${uuid}` : `${root}/${id}.json`;
-}
-
 function base(badge) {
-  return Object.assign(facetBase(), { id: id(badge), type: 'BadgeClass' });
+  return Object.assign(
+    facetBase(),
+    { id: `${root}/${badge.id}.json`, type: 'BadgeClass' }
+  );
 }
 
 module.exports = {
