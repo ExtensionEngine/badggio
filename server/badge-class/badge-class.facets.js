c43070b133033f07bf70a24c975d46298fbad444
Nediljko Krstic <nediljko@extensionengine.com>
2018-07-11 13:35:06 +0200

    Address PR comments

--- server/badge-class/badge-class.facets.js
+++ server/badge-class/badge-class.facets.js
@@ -3,43 +3,28 @@
 const paths = require('./badge-class.paths');
 const pickBy = require('lodash/pickBy');
 const { base: facetBase } = require('../common/facets');
-const { issuer } = require('../config');
+const { criteria } = require('../criteria-class/criteria-class.facets');
+const { image } = require('../image-class/image-class.facets');
+const { issuer: { publicKey } } = require('../config');
+const { profile: issuer } = require('../issuer/issuer.facets');
 const { SERVER_URL } = process.env;
 
 const root = SERVER_URL + paths.root;
 
 function badge(badge) {
-  const { name, description, imageCaption, imageAuthorIri, tags } = badge;
+  const { name, description, tags } = badge;
 
-  return Object.assign(
-    base(badge),
-    { name, description, image: imageIri(badge) },
-    { criteria: criteriaClass(badge) },
-    pickBy({ imageCaption, imageAuthorIri, tags }),
-    { issuer: issuer.issuerUrl }
+  return pickBy(
+    Object.assign(base(badge), { name, description, image, criteria, tags, issuer })
   );
 }
 
-function imageIri({ id }) {
-  return `${root}/${id}${paths.image}`;
-}
-
-function criteriaClass({ id, criteriaNarrative }) {
-  return {
-    id: `${root}/${id}${paths.criteria}`,
-    narrative: criteriaNarrative
-  };
-}
-
-function badgeClassIri({ id }) {
-  return `${root}/${id}.json`;
+function badgeClassIri({ id, uuid }) {
+  return publicKey ? `urn:uuid:${uuid}` : `${root}/${id}.json`;
 }
 
 function base(badge) {
-  return Object.assign(
-    facetBase(),
-    { id: badgeClassIri(badge), type: 'BadgeClass' }
-  );
+  return Object.assign(facetBase(), { id: badgeClassIri(badge), type: 'BadgeClass' });
 }
 
 module.exports = {
