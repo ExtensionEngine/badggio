53cc402004acb83f231ee14f910a481077c04758
Nediljko Krstic <nediljko@extensionengine.com>
2018-07-19 11:38:35 +0200

    Add loadBadge middleware where needed

--- server/badge-class/index.js
+++ server/badge-class/index.js
@@ -6,18 +6,18 @@ const paths = require('./badge-class.paths');
 const apiRouter = require('express').Router();
 const badgingRouter = require('express').Router();
 
-const { badge, create, criteria, decodeImage, encodeImages, image, list, patch } = ctrl;
+const { badge, create, criteria, decodeImage, encodeImages, image, list, loadBadge, patch } = ctrl;
 
 apiRouter
   .use(auth)
   .get('/', list, encodeImages)
   .post('/', decodeImage, create)
-  .patch('/:id', decodeImage, patch);
+  .patch('/:id', decodeImage, loadBadge, patch);
 
 badgingRouter
-  .get('/:id.json', badge)
-  .get(`/:id${paths.image}`, image)
-  .get(`/:id${paths.criteria}`, criteria);
+  .get('/:id.json', loadBadge, badge)
+  .get(`/:id${paths.image}`, loadBadge, image)
+  .get(`/:id${paths.criteria}`, loadBadge, criteria);
 
 module.exports = {
   path: paths.root,
