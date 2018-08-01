aaba6e3d53ad72df6796879ab1b025c603e89164
mcapeta <marin.capeta@gmail.com>
2018-08-01 13:45:34 +0200

    Add Assertion main component

---
+++ client/components/assertions/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="title">Assertions</h1>
+    <recipient-select></recipient-select>
+  </div>
+</template>
+
+<script>
+import RecipientSelect from '../recipients/Select';
+
+export default {
+  name: 'assertion-list',
+  components: { RecipientSelect }
+};
+</script>
