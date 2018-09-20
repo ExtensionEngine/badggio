68aa9e27b8e8fd3f346f038c0c22e20ee1a75cc6
mcapeta <marin.capeta@gmail.com>
2018-09-20 14:35:59 +0200

    Create Badge select component

---
+++ client/components/badges/Select.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-select
+    :disabled="disabled"
+    :multiple="false"
+    :options="badges"
+    :value="selected"
+    @remove="deselect"
+    @select="select"
+    name="Badge"
+    trackBy="id">
+  </v-select>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+import map from 'lodash/map';
+import values from 'lodash/values';
+import VSelect from '@/components/common/form/VSelect';
+
+const badgeMapper = badge => {
+  const { id, image, name, imageCaption, description } = badge;
+  return {
+    id,
+    image,
+    title: name,
+    alt: imageCaption,
+    desc: description
+  };
+};
+
+export default {
+  name: 'badge-select',
+  props: {
+    disabled: { type: Boolean, default: false },
+    badge: { type: Object, default: null }
+  },
+  data() {
+    return {
+      selected: this.badge
+    };
+  },
+  computed: {
+    ...mapState('badges', { badges: ({ items }) => map(values(items), badgeMapper) })
+  },
+  methods: {
+    ...mapActions('badges', ['fetch']),
+    deselect() {
+      this.selected = null;
+    },
+    select(badge) {
+      this.selected = badge;
+    }
+  },
+  mounted() {
+    this.fetch();
+  },
+  components: { VSelect }
+};
+</script>
