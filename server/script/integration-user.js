4a8901b209c94909209c2ce1fc7ae980ba4f3c92
mcapeta <marin.capeta@gmail.com>
2018-09-25 12:16:47 +0200

    Remove password requirement for integration users

--- server/script/integration-user.js
+++ server/script/integration-user.js
@@ -21,13 +21,8 @@ const action = actions[process.argv[2]];
 const questions = [{
   type: 'input',
   name: 'username',
-  message: 'Enter integration name:'
-}, {
-  type: 'password',
-  mask: '*',
-  name: 'password',
-  message: 'Enter password:',
-  validate: getValidator(User, 'password')
+  message: 'Enter integration name:',
+  validate: getValidator(User, 'username')
 }];
 
 prompt(questions)
@@ -36,16 +31,14 @@ prompt(questions)
   .catch(err => console.error(err.message) || 1)
   .then((code = 0) => process.exit(code));
 
-function create({ username, password }) {
-  return User.create({ username, password, role: INTEGRATION })
+function create({ username }) {
+  return User.create({ username, role: INTEGRATION })
     .then(user => console.log(`Integration created: ${user.username}`) || user);
 }
 
-function get({ username, password }) {
+function get({ username }) {
   return User.findOne({ where: { username, role: INTEGRATION } })
-    .then(user => user || Promise.reject(Error(`Integration "${username}" does not exist.`)))
-    .then(user => user.authenticate(password))
-    .then(user => user || Promise.reject(Error('Invalid password.')));
+    .then(user => user || Promise.reject(Error(`Integration "${username}" does not exist.`)));
 }
 
 function getValidator(Model, attribute) {
