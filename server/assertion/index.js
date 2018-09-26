413295add04311cc7ca2b31e41f09c73e8a2de55
mcapeta <marin.capeta@gmail.com>
2018-09-26 22:13:50 +0200

    Add role based auth rules

--- server/assertion/index.js
+++ server/assertion/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { permit } = require('../common/auth/mw');
 const auth = require('../common/auth').authenticate('jwt');
 const ctrl = require('./assertion.controller');
 const paths = require('./assertion.paths');
@@ -7,13 +8,14 @@ const apiRouter = require('express').Router();
 const badgingRouter = require('express').Router();
 
 const { badgeAssertion, create, image, list, loadAssertion, patch } = ctrl;
+const INTEGRATION = 'INTEGRATION';
 
 apiRouter
   .use(auth)
   .param('id', loadAssertion)
   .get('/', list)
-  .post('/', create)
-  .patch('/:id', patch);
+  .post('/', permit(INTEGRATION), create)
+  .patch('/:id', permit(INTEGRATION), patch);
 
 badgingRouter
   .param('id', loadAssertion)
