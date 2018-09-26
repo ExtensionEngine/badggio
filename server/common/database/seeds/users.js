90b533e2563e672b3e534c7c620df74ce3b326a8
mcapeta <marin.capeta@gmail.com>
2018-09-26 16:12:39 +0200

    Revert "Add integration users into seed"
    This reverts commit 82fd3741ccb16d197210ce42fb138ff2b3f0fd34.

--- server/common/database/seeds/users.js
+++ server/common/database/seeds/users.js
@@ -1,13 +1,9 @@
 'use strict';
 
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const pick = require('lodash/pick');
 const Promise = require('bluebird');
 const { auth: config = {} } = require('../../../config');
 const { role } = require('../../../../common/config');
-
-const { ADMIN, INTEGRATION } = role;
 
 const now = new Date();
 const users = [{
@@ -15,7 +11,7 @@ const users = [{
   last_name: 'Example',
   username: 'admin@example.org',
   password: 'admin123',
-  role: ADMIN,
+  role: role.ADMIN,
   created_at: now,
   updated_at: now
 }, {
@@ -23,39 +19,20 @@ const users = [{
   last_name: 'Example',
   username: 'admin2@example.org',
   password: 'admin123',
-  role: ADMIN,
-  created_at: now,
-  updated_at: now
-}, {
-  username: 'lms',
-  password: 'admin123',
-  role: INTEGRATION,
-  created_at: now,
-  updated_at: now
-}, {
-  username: 'tailor',
-  password: 'admin123',
-  role: INTEGRATION,
+  role: role.ADMIN,
   created_at: now,
   updated_at: now
 }];
 
 module.exports = {
   up(queryInterface, Sequelize) {
-    return Promise.map(users, user => {
-      if (user.role === INTEGRATION) user.token = createToken(user);
-      return encryptPassword(user);
-    }).then(users => queryInterface.bulkInsert('user', users, {}));
+    return Promise.map(users, user => encryptPassword(user))
+      .then(users => queryInterface.bulkInsert('user', users, {}));
   },
   down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('user', null, {});
   }
 };
-
-function createToken(user) {
-  const payload = pick(user, ['id', 'username']);
-  return jwt.sign(payload, config.secret);
-}
 
 function encryptPassword(user) {
   return bcrypt.hash(user.password, config.saltRounds)
