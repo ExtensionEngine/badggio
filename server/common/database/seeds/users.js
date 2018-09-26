41b85a261a439f80658d668327343d5f36141330
mcapeta <marin.capeta@gmail.com>
2018-09-26 16:51:16 +0200

    Add integration users to seed script

--- server/common/database/seeds/users.js
+++ server/common/database/seeds/users.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const bcrypt = require('bcrypt');
+const hasha = require('hasha');
 const Promise = require('bluebird');
 const { auth: config = {} } = require('../../../config');
-const { role } = require('../../../../common/config');
+const { role: { ADMIN, INTEGRATION } } = require('../../../../common/config');
 
 const now = new Date();
 const users = [{
@@ -11,7 +12,7 @@ const users = [{
   last_name: 'Example',
   email: 'admin@example.org',
   password: 'admin123',
-  role: role.ADMIN,
+  role: ADMIN,
   created_at: now,
   updated_at: now
 }, {
@@ -19,14 +20,28 @@ const users = [{
   last_name: 'Example',
   email: 'admin2@example.org',
   password: 'admin123',
-  role: role.ADMIN,
+  role: ADMIN,
+  created_at: now,
+  updated_at: now
+}, {
+  first_name: 'lms',
+  role: INTEGRATION,
+  created_at: now,
+  updated_at: now
+}, {
+  first_name: 'tailor',
+  role: INTEGRATION,
   created_at: now,
   updated_at: now
 }];
 
 module.exports = {
   up(queryInterface, Sequelize) {
-    return Promise.map(users, user => encryptPassword(user))
+    return Promise.map(users, user => {
+      if (user.role !== INTEGRATION) encryptPassword(user);
+      else user.email = `${sha1(user.first_name, 14)}@integration.localhost`;
+      return user;
+    })
       .then(users => queryInterface.bulkInsert('user', users, {}));
   },
   down(queryInterface, Sequelize) {
@@ -38,4 +53,9 @@ function encryptPassword(user) {
   return bcrypt.hash(user.password, config.saltRounds)
     .then(password => (user.password = password))
     .then(() => user);
+}
+
+function sha1(input, length) {
+  const hash = hasha(input, { algorithm: 'sha1' });
+  return hash.substring(hash.length - length);
 }
