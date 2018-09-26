f63ad171759d28f8e84049f77553009996a21d8c
mcapeta <marin.capeta@gmail.com>
2018-09-26 16:12:52 +0200

    Revert "Change seed according to db column name change"
    This reverts commit a88c1c0927ac86aade754888b98aea904f9f90df.

--- server/common/database/seeds/users.js
+++ server/common/database/seeds/users.js
@@ -9,7 +9,7 @@ const now = new Date();
 const users = [{
   first_name: 'Admin',
   last_name: 'Example',
-  username: 'admin@example.org',
+  email: 'admin@example.org',
   password: 'admin123',
   role: role.ADMIN,
   created_at: now,
@@ -17,7 +17,7 @@ const users = [{
 }, {
   first_name: 'Admin2',
   last_name: 'Example',
-  username: 'admin2@example.org',
+  email: 'admin2@example.org',
   password: 'admin123',
   role: role.ADMIN,
   created_at: now,
