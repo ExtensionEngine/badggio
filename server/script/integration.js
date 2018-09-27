45d82b8b9626794fcd483835b3e95c77e24d480d
Dario Vladovic <d.vladimyr@gmail.com>
2018-09-27 13:46:55 +0200

    Refactor integration management cli :wrench:
    - implement list command

--- server/script/integration.js
+++ server/script/integration.js
@@ -3,38 +3,77 @@
 const { getValidator, setLogging } = require('../common/database/helpers');
 const { Integration } = require('../common/database');
 const { prompt } = require('inquirer');
+const isEmail = require('is-email-like');
 
 setLogging(Integration, false);
 
-const actions = {
-  create,
-  token: findOne,
-  default: findOne
+const defaultDomain = domain => input => {
+  if (input.includes('@')) return input;
+  return `${input}@${domain}`;
 };
-const action = actions[process.argv[2]] || actions.default;
+const line = (char = '-', length = Math.min(process.stdout.columns, 80)) => {
+  let result = '';
+  while (length) {
+    result += char;
+    length--;
+  }
+  return result;
+};
 
-const questions = [{
-  type: 'input',
-  name: 'name',
-  message: 'Enter integration name:',
-  validate: getValidator(Integration, 'name')
-}];
+const template = (integration, token = false) => `
+# ${integration.name}
+${line()}
+Id:    ${integration.id}
+Email: ${integration.email}
+${(token && `Token: ${integration.token}\n`) || ''}`;
 
-prompt(questions)
-  .then(data => console.log() || action(data))
-  .then(integration => console.log(`Integration token: ${integration.token}`))
+const commands = {
+  list,
+  create,
+  token: getToken,
+  default: list
+};
+const action = commands[process.argv[2]] || commands.default;
+
+action()
   .catch(err => console.error(err.message) || 1)
   .then((code = 0) => process.exit(code));
 
-function create({ name }) {
-  return Integration.create({ name })
-    .then(integration => console.log(`Integration created: ${name}`) || integration);
+function list() {
+  return Integration.findAll({ where: { role: Integration.role } })
+    .each(integration => console.log(template(integration)));
 }
 
-function findOne({ name }) {
-  return Integration.findOne({ where: { name, role: Integration.role } })
-    .then(integration => {
-      if (integration) return integration;
-      return Promise.reject(new Error(`Integration "${name}" does not exist.`));
+function create() {
+  const question = {
+    type: 'input',
+    name: 'name',
+    message: 'Enter integration name:',
+    validate: getValidator(Integration, 'name')
+  };
+  return prompt([question])
+    .then(({ name }) => Integration.create({ name }))
+    .then(integration => console.log(template(integration, true)));
+}
+
+function getToken() {
+  const question = {
+    type: 'input',
+    name: 'email',
+    message: 'Enter integration email:',
+    validate: isEmail,
+    filter: defaultDomain('integration.localhost')
+  };
+  return prompt([question])
+    .then(({ email }) => {
+      email = email.trim().toLowerCase();
+      return Promise.all([
+        Integration.findOne({ where: { email, role: Integration.role } }),
+        email
+      ]);
+    })
+    .then(([integration, email]) => {
+      if (integration) return console.log(template(integration, true));
+      return Promise.reject(new Error(`Integration "${email}" does not exist.`));
     });
 }
