0c73ceacc90b85de55b575ad5acc3cb93fe35a6e
mcapeta <marin.capeta@gmail.com>
2018-10-09 16:38:12 +0200

    Refactor tag input component

--- client/components/common/form/VTagsInput.vue
+++ client/components/common/form/VTagsInput.vue
@@ -1,23 +1,19 @@
 <template>
-  <div class="field">
-    <label class="label">{{ label }}</label>
-    <button @click.prevent="addTag();" class="button is-small">
-      <span class="icon has-text-primary mdi mdi-plus-circle mdi-24px" />
-    </button>
-    <div v-for="(tag, index) in value" :key="index" class="control">
-      <input
-        v-bind="$attrs"
-        :value="tag"
-        :type="type"
+  <div>
+    <div class="tag-input">
+      <v-input
         :name="name"
-        :placeholder="'Tag'"
-        @input="input($event, index);"
-        class="input">
-      <button
-        @click.prevent="removeTag(index);"
-        class="button is-small is-pulled-right">
-        <span class="icon mdi mdi-close" />
+        v-bind="$attrs"
+        v-model="currentTag"
+        @keydown.enter.prevent="addTag"/>
+      <button @click.prevent="addTag" class="button is-small">
+        <span class="icon has-text-primary mdi mdi-plus-circle mdi-24px" />
       </button>
+    </div>
+    <div class="tags control">
+      <span v-for="(tag, index) in value" :key="index" class="tag is-primary">{{ tag }} &nbsp;
+        <button @click.prevent="removeTag(index)" class="delete is-small"></button>
+      </span>
     </div>
   </div>
 </template>
@@ -36,7 +32,10 @@ export default {
     value: { type: Array, default: () => [] }
   },
   data() {
-    return { tags: clone(this.value) };
+    return {
+      currentTag: '',
+      tags: clone(this.value)
+    };
   },
   computed: {
     label() {
@@ -44,12 +43,9 @@ export default {
     }
   },
   methods: {
-    input({ target }, index) {
-      this.tags.splice(index, 1, target.value);
-      this.update();
-    },
     addTag() {
-      this.tags.push('');
+      this.tags.push(this.currentTag);
+      this.currentTag = '';
       this.update();
     },
     removeTag(index) {
@@ -70,24 +66,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.label {
-  display: inline-block;
-}
-
-.button {
-  border:  0;
-  background-color: transparent;
-}
-
-.input {
-  max-width: 95%;
-}
-
-.control {
-  margin-bottom: 0.75rem;
+.tag-input {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
 
   .button {
-    margin-top: 5px;
+    margin-left: 10px;
+    border:  0;
+    background-color: transparent;
   }
 }
 </style>
