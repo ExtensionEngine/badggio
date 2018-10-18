2e8eb791fae1a0f3f060668bdd13180f0f64cb10
Dario Vladovic <d.vladimyr@gmail.com>
2018-10-18 22:44:41 +0200

    Tweak navbar styles :art:

--- client/components/common/Navbar.vue
+++ client/components/common/Navbar.vue
@@ -4,8 +4,8 @@
     role="navigation"
     aria-label="main navigation">
     <div class="navbar-brand">
-      <router-link to="/" class="navbar-item">BADGGIO</router-link>
-      <div class="navbar-item is-hidden-desktop is-right">{{ user.email }}</div>
+      <router-link to="/" class="item-home navbar-item has-text-weight-semibold">BADGGIO</router-link>
+      <div class="item-email navbar-item is-hidden-desktop has-text-grey">{{ user.email }}</div>
       <a :class="{ 'is-active': showMenu }" @click="showMenu = !showMenu" class="navbar-burger">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
@@ -15,9 +15,9 @@
     <div :class="{ 'is-active': showMenu }" class="navbar-menu">
       <div v-if="user" class="navbar-end">
         <router-link to="/badges" class="navbar-item">Badges</router-link>
-        <div class="navbar-item is-hidden-touch">{{ user.email }}</div>
+        <div class="item-email navbar-item is-hidden-touch has-text-grey">{{ user.email }}</div>
         <div class="navbar-item has-dropdown is-hoverable">
-          <div class="navbar-item is-hidden-touch mdi mdi-settings mdi-24px"></div>
+          <a class="btn-menu navbar-item is-hidden-touch mdi mdi-dots-vertical mdi-24px"></a>
           <div class="navbar-dropdown is-right is-boxed">
             <router-link to="/users" class="navbar-item">
               <span>Access Rights</span>
@@ -53,33 +53,77 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '~bulma/sass/utilities/initial-variables.sass';
+@import '~bulma/sass/utilities/all';
 
-.mdi-logout {
-  margin-left: -5px;
-}
+@media (min-width: $desktop) {
+  .item-email {
+    user-select: none;
+  }
 
-@media (min-width: 1024px) {
+  .btn-menu {
+    margin-left: -0.25rem;
+
+    &:hover {
+      color: $link-hover;
+      background-color: #e8e8e8;
+    }
+  }
+
   .navbar-dropdown {
-    .has-icon {
-      display: block;
+    $border-color: lighten($grey-lighter, 5%);
+
+    margin-top: 4px;
+    border-radius: 0;
+    box-shadow: 0 8px 8px rgba(10, 10, 10, 0.1);
+    border: 1px solid $border-color;
+    border-top: none;
+    border-right: none;
+
+    .navbar-item {
+      display: inline-block;
+      width: 100%;
+    }
+
+    .navbar-divider {
+      height: 1px;
+      background: $border-color;
     }
   }
 }
 
-@media (max-width: 1023px) {
+@media (max-width: $desktop - 1px) {
+  .navbar-burger {
+    margin-left: 0;
+  }
+
+  .item-home {
+    flex-grow: 0;
+    margin-right: auto;
+  }
+
+  .navabar-menu .item-email {
+    flex-grow: 0;
+  }
+
   .navbar-dropdown {
-    &:before {
+    padding: 0.5rem 0.75rem;
+    font-size: 1rem;
+
+    &::before {
       content: "";
       display: block;
       height: 1px;
-      width: 96%;
-      margin-left: 2%;
+      margin-bottom: 0.5rem;
       background-color: $grey-lighter;
     }
+
     .navbar-item {
-      padding-right: 1rem;
-      padding-left: 1rem;
+      margin: 0 -0.75rem;
+      padding: 0.5rem 1rem;
+    }
+
+    .mdi-logout {
+      margin-left: -0.25px;
     }
   }
 }
