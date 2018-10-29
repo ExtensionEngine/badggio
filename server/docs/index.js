61738e065e3bf816f4fa3ae46edef068c69703f9
mcapeta <marin.capeta@gmail.com>
2018-10-29 16:25:46 +0100

    Redefine docs api endpoint
    - ditch `swagger-ui-express`
    - expose docs in `.json`, `.js` and `.yaml` format

--- server/docs/index.js
+++ server/docs/index.js
@@ -1,26 +1,11 @@
 'use strict';
 
-const fs = require('fs');
-const logger = require('../common/logger')();
-const path = require('path');
+const ctrl = require('./docs.controller');
 const router = require('express').Router();
-const swagger = require('swagger-ui-express');
-const yaml = require('js-yaml');
 
-const api = loadYaml('../../docs/api.yaml');
-
-router.use(swagger.serve, swagger.setup(api));
+router.get('/api.:type(js|json|yaml)', ctrl.docs);
 
 module.exports = {
   path: '/docs',
   router
 };
-
-function loadYaml(filepath) {
-  filepath = path.join(__dirname, filepath);
-  try {
-    return yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
-  } catch (err) {
-    logger.error(err, 'Error loading API documentation');
-  }
-}
